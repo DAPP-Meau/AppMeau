8ed0ae27227518f330b1e73c15a57123623c9d92
Wagner <wagnerweb2010@gmail.com>
2024-04-11 02:34:10 -0300

    fix: Reviewed and fixed some code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,30 @@
 import { Text } from "react-native";
 import { Stack } from "expo-router/stack";
 import { useFonts } from 'expo-font';
-import { fonts } from "@/constants";
+import { fonts } from "@/assets";
+import { SplashScreen } from "expo-router";
+import { useEffect } from "react";
 
-
+SplashScreen.preventAutoHideAsync();
 
 export default function AppLayout() {
-  let [fontsLoaded] = useFonts(fonts);
-  if(!fontsLoaded){
+  let [fontsLoaded, error] = useFonts(fonts);
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
     return null;
   }
+  
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
