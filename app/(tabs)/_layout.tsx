a9ef78dd5e0fb7cb5db23cab3f1a1d916c17d805
Pedro Rafael Faria Ferreira <pedro.rafael@sempreceub.com>
2024-04-10 23:37:45 -0300

    Implementar todas as fontes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,24 +1,27 @@
 import React from "react";
+import { StyleSheet, TouchableOpacity, Text, View, Image } from "react-native";
 import Colors from "@/constants/Colors";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Drawer } from "expo-router/drawer";
 
 export default function TabLayout() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
+    <GestureHandlerRootView style={{flex:1}}>
       <Drawer>
         <Drawer.Screen
           name="index"
           options={{
             drawerLabel: "Tela Inicial",
             title: "",
-            headerStyle: { backgroundColor: Colors.background.default },
+            headerTitleStyle: {fontFamily:'Roboto_Medium'},
+            headerStyle: { backgroundColor: Colors.background.default},
           }}
         />
         <Drawer.Screen
           name="login"
           options={{
             title: "Login",
+            headerTitleStyle: {fontFamily:'Roboto_Medium'},
             headerStyle: { backgroundColor: Colors.tintLight.blue2 },
           }}
         />
@@ -26,6 +29,7 @@ export default function TabLayout() {
           name="createLogin"
           options={{
             title: "Cadastrar Pessoal",
+            headerTitleStyle: {fontFamily:'Roboto_Medium'},
             headerStyle: { backgroundColor: Colors.tintLight.blue2 },
           }}
         />
