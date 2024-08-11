e0cec554c249d1e76d5d18728ac0fa73af575cb2
Wagner Batalha <wagnerweb2010@gmail.com>
2024-08-10 21:56:53 -0300

    Criado configuranção de ambiente básica

--- constants/firebaseConfig.ts
+++ constants/firebaseConfig.ts
@@ -1,8 +1,8 @@
 export const firebaseAppConfig = {
-  apiKey: "AIzaSyAdR3vDtZ8a8QnvyyHSp3K7-9qvVO1LhLk",
-  authDomain: "meau-app-1b023.firebaseapp.com",
+  apiKey: process.env.EXPO_PUBLIC_API_URL,
+  authDomain: process.env.EXPO_PUBLIC_AUTHDOMAIN,
   // The value of `databaseURL` depends on the location of the database
-  projectId: "meau-app-1b023",
-  storageBucket: "meau-app-1b023.appspot.com",
-  appId: "813508935570",
+  projectId: process.env.EXPO_PUBLIC_PROJECTID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGEBUCKET,
+  appId: process.env.EXPO_PUBLIC_APPID,
 };
