d64320a08f5d7a0c77c3fe1c44ea9c3eb38e098e
Wagner Batalha <wagnerweb2010@gmail.com>
2024-08-15 16:46:57 -0300

    doc: Comentários e descrições melhors

--- app/Screens/Chat.tsx
+++ app/Screens/Chat.tsx
@@ -1,5 +1,5 @@
 import { StackScreenProps } from "@react-navigation/stack"
-import React, { useState, useCallback, useEffect, useContext } from "react"
+import React, { useState, useEffect, useContext, useCallback } from "react"
 import { GiftedChat, IMessage } from "react-native-gifted-chat"
 import { RootStackParamList } from "../Navigation/RootStack"
 import {
@@ -24,7 +24,7 @@ type Props = StackScreenProps<RootStackParamList, "chat">
 export default function Chat({ route }: Props) {
   const firebaseApp = useContext(FirebaseAppContext)
   const db = getFirestore(firebaseApp)
-  const loggedInUser = getCurrentUserUID(firebaseApp)
+  const loggedInUserID = getCurrentUserUID(firebaseApp)
   const [loggedInUserDocument, setLoggedInUserDocument] = useState<User>()
   const [messages, setMessages] = useState<Array<IMessage>>([])
 
@@ -34,34 +34,40 @@ export default function Chat({ route }: Props) {
     collectionPaths.rooms_messages,
   )
 
+  // Pegar documento do usuário logado
   useEffect(() => {
-    if (loggedInUser) {
+    if (loggedInUserID) {
       callback()
     }
+
     async function callback() {
-      if (!loggedInUser) throw new Error("No logged in user!")
-      const tempUser = await getUserAction(loggedInUser, firebaseApp)
+      if (!loggedInUserID) throw new Error("No logged in user!")
+      const tempUser = await getUserAction(loggedInUserID, firebaseApp)
       setLoggedInUserDocument(tempUser)
     }
-  }, [loggedInUser])
+  }, [loggedInUserID])
 
+  // Event Listener de mensagens novas do firebase.
   useEffect(() => {
     const q = query(
       messagesCollectionReference,
-      limit(100),
       orderBy("createdAt", "desc"),
+      limit(100),
     )
+    // Possível optmização: pegar mensagens iniciais com uma requisição normal
+    // e depois registrar esse event listener para pegar só as mensagens novas.
     const unsubscribe = onSnapshot(q, (snapshot) => {
       const tempMessages: IMessage[] = []
       for (const doc of snapshot.docs) {
         const data = doc.data() as IMessage
-        console.log(data)
-        if (!data.createdAt) continue
+        const { createdAt, ...restOfMessage } = data
+        // On snapshot atualiza para mensagens locais, quando isso acontece
+        // createdAt é undefined, pois as horas são criadas apenas quando
+        // o documento criado em onSend chega ao servidor
+        if (!createdAt) continue
         tempMessages.push({
-          _id: doc.id,
-          createdAt: data.createdAt.toDate(),
-          text: data.text,
-          user: data.user,
+          ...restOfMessage,
+          createdAt: data.createdAt.toDate(), // Erro de tipo, coerção necessária
         })
       }
       setMessages(tempMessages)
@@ -70,20 +76,28 @@ export default function Chat({ route }: Props) {
     return () => unsubscribe()
   }, [firebaseApp])
 
+  // Callback de mensagens novas
   const onSend = useCallback(
     async (newMessages: IMessage[] = []) => {
       const writeBatch = async () => {
-        const batch = newMessages.map(async (message) => {
-          await addDoc(messagesCollectionReference, {
-            text: message.text,
+        const batch = newMessages.map((message) => {
+          // Nós realmente queremos ignorar createdAt
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
+          const { createdAt: _, ...restOfMessage } = message
+
+          const pushDoc: IMessage = {
+            // @ts-expect-error 2339 Firebase armazena dados numa classe != Date
             createdAt: serverTimestamp(),
-            user: message.user,
-          })
+            ...restOfMessage,
+          }
+
+          return addDoc(messagesCollectionReference, pushDoc)
         })
 
+        // batch é uma lista de promises. Usando Promise.all podemos executar
+        // paralelamente todos os pedidos.
         await Promise.all(batch)
       }
-
       await writeBatch()
       setMessages((previousMessages) =>
         GiftedChat.append(previousMessages, newMessages),
@@ -97,8 +111,8 @@ export default function Chat({ route }: Props) {
       messages={messages}
       onSend={(messages) => onSend(messages)}
       user={{
-        _id: 1,
-        name: loggedInUserDocument?.person.fullName ?? "none",
+        _id: loggedInUserID ?? 0,
+        name: loggedInUserDocument?.person.fullName ?? "null",
       }}
     />
   )
