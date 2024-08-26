d9dc67f2c29e63cd2ab984f398e050b34aed74be
Wagner Batalha <wagnerweb2010@gmail.com>
2024-08-26 13:47:43 -0300

    configurar variáveis de desenvolvimento

--- constants/firebaseConfig.ts
+++ constants/firebaseConfig.ts
@@ -1,8 +1,12 @@
-export const firebaseAppConfig = {
-  apiKey: process.env.EXPO_PUBLIC_API_URL,
+import { FirebaseOptions } from "firebase/app"
+
+export const firebaseAppConfig: FirebaseOptions = {
+  apiKey: process.env.EXPO_PUBLIC_APIKEY,
   authDomain: process.env.EXPO_PUBLIC_AUTHDOMAIN,
-  // The value of `databaseURL` depends on the location of the database
+  databaseURL: process.env.EXPO_PUBLIC_DATABASEURL,
   projectId: process.env.EXPO_PUBLIC_PROJECTID,
   storageBucket: process.env.EXPO_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGINGSENDERID,
   appId: process.env.EXPO_PUBLIC_APPID,
-};
+  measurementId: process.env.EXPO_PUBLIC_MEASUREMENTID,
+}
