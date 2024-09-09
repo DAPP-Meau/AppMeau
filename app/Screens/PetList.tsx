cced602fc33267a60d1d77a94a80e46f83f27e49
Pedro Rafael Faria Ferreira <pedro.rafael@sempreceub.com>
2024-09-08 23:07:15 -0300

    finalizado as transações dos pets

--- app/Screens/PetList.tsx
+++ app/Screens/PetList.tsx
@@ -5,12 +5,14 @@ import React, { useContext } from "react"
 
 export default function PetList() {
   const user = useContext(LoggedInUserContext)
-  if (!user?.id) {
-    console.error("userID vazio para rodar lista de pets!");
-  }
+
   return (
-    <PetListComponent
-      query={[where(user?.id ?? "", "not-in", "rejectedUsersList")]}
-    />
+    <>
+      {user?.id && 
+        <PetListComponent
+          
+        />
+      }
+    </>
   )
 }
